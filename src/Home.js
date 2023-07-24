d9364a121a29ddaf64a7496be650d16085081c50
masu <yasumoto.7991mm@gmail.com>
2023-07-24 15:33:21 +0700

    update skill-content

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Home.css";
-import javaLogo from "./java.svg"; // 画像をimport
+import javaLogo from "./Picture/java.svg"; // 画像をimport
+import javaScriptLogo from "./Picture/javascript.svg"; // 画像をimport
+import reactLogo from "./Picture/react.svg"; // 画像をimport
+import cLanguageLogo from "./Picture/clanguage.png"; // 画像をimport
+import pythonLogo from "./Picture/python.svg"; // 画像をimport
 
 const AboutMe = () => (
   <div className="section about-me">
@@ -11,15 +15,36 @@ const AboutMe = () => (
 
 const Skill = () => (
   <div className="section skill">
-    <div className="title">Skill</div>
-    <div className="card">
-      <img src={javaLogo} alt="java" />
-      <div>Java</div>
-      <div>経験年数：4年</div>
+    <div className="skill-content">
+      <div className="title">Skill</div>
+      <div className="cardList">
+        <div className="card">
+          <img src={javaLogo} alt="java" />
+          <div>Java</div>
+          <div>経験年数：4年</div>
+        </div>
+        <div className="card">
+          <img src={javaScriptLogo} alt="javaScript" />
+          <div>JavaScript</div>
+          <div>経験年数：1年</div>
+        </div>
+        <div className="card">
+          <img src={reactLogo} alt="react" />
+          <div>React</div>
+          <div>経験年数：1年</div>
+        </div>
+        <div className="card">
+          <img src={cLanguageLogo} alt="clanguage" />
+          <div>C</div>
+          <div>経験年数：1年</div>
+        </div>
+        <div className="card">
+          <img src={pythonLogo} alt="python" />
+          <div>React</div>
+          <div>経験年数：1年</div>
+        </div>
+      </div>
     </div>
-    <div className="card">JavaScript</div>
-    <div className="card">C</div>
-    <div className="card">Python</div>
   </div>
 );
 
