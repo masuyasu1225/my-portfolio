4f50eef39a97869dc2df6d9bb4c270ebff8c5ff8
masu <yasumoto.7991mm@gmail.com>
2023-07-24 12:26:20 +0700

    update template

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,31 @@
 import React from "react";
 import "./Home.css";
+import javaLogo from "./java.svg"; // 画像をimport
 
 const AboutMe = () => (
   <div className="section about-me">
-    <h2>About Me</h2>
+    <div className="title">About Me</div>
     <p>ここに自己紹介文を記載</p>
   </div>
 );
 
 const Skill = () => (
   <div className="section skill">
-    <h2>Skill</h2>
-    <p>ここにスキルを記載</p>
+    <div className="title">Skill</div>
+    <div className="card">
+      <img src={javaLogo} alt="java" />
+      <div>Java</div>
+      <div>経験年数：4年</div>
+    </div>
+    <div className="card">JavaScript</div>
+    <div className="card">C</div>
+    <div className="card">Python</div>
   </div>
 );
 
 const Service = () => (
   <div className="section service">
-    <h2>Service</h2>
+    <div className="title">Service</div>
     <p>ここに提供するサービスを記載</p>
   </div>
 );
