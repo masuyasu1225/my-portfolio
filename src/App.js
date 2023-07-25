27d77cd07758fcc02b8e12aa97f65d3f38f82a8a
masu <yasumoto.7991mm@gmail.com>
2023-07-25 10:54:31 +0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Home.js";
+import Navbar from "./Navbar";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
