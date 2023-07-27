d8974a8704593e9446924aa1cc0339d2fddb5f70
masu <yasumoto.7991mm@gmail.com>
2023-07-27 09:32:25 +0700

    update App.jsRouter

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import "./App.css";
 import Home from "./Home.js";
 import Navbar from "./Navbar";
 
-const homeUrl = process.env.PUBLIC_URL;
-
 function App() {
   return (
     <Router basename="/my-portfolio">
