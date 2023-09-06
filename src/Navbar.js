d53e75df0a7fb997bb3017de9c1fdb6ed57d0fac
masu <yasumoto.7991mm@gmail.com>
2023-09-06 10:04:47 +0900

    Update responsive design for navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav>
-      <>
+      <div className="menu-icon" onClick={() => setIsOpen(!isOpen)}>
+        ☰
+      </div>
+      <div className={`nav-links ${isOpen ? "show" : ""}`}>
         <Link to="/">ホーム</Link>
         <a href="#about-me">About Me</a>
         <a href="#skill">Skill</a>
         <a href="#service">Service</a>
         <a href="#academic">Academic</a>
         <a href="#contact-form">Contact</a>
-      </>
+      </div>
     </nav>
   );
 };
