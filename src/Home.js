f632ec37791fc201e4121187dfc5f22c006b047b
masu <yasumoto.7991mm@gmail.com>
2023-09-06 11:55:48 +0900

    Update Aboutme

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.css";
+import masui from "./Picture/masui.jpg";
 import githubLogo from "./Picture/github-mark.svg";
 import javaLogo from "./Picture/java.svg";
 import javaScriptLogo from "./Picture/javascript.svg";
@@ -21,18 +22,23 @@ import ContactForm from "./ContactForm";
 const AboutMe = () => (
   <div className="section about-me" id="about-me">
     <div className="title">About Me</div>
-    <p>
-      業務系のシステム開発を行うSIerの会社に1年2か月ほど勤務し、設計・開発・テストを一通り行いました。
-    </p>
-    <p>Webエンジニアへの転職を目指しています。</p>
-    <a
-      href="https://github.com/masuyasu1225"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <img src={githubLogo} alt="github" />
-      githubリンク
-    </a>
+    <div className="card">
+      <img src={masui} alt="masui" />
+      <div className="card-content">
+        <p>
+          業務系のシステム開発を行うSIerの会社に1年2か月ほど勤務し、設計・開発・テストを一通り行いました。
+        </p>
+        <p>Webエンジニアへの転職を目指しています。</p>
+        <a
+          href="https://github.com/masuyasu1225"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={githubLogo} alt="github" />
+          githubリンク
+        </a>
+      </div>
+    </div>
   </div>
 );
 
