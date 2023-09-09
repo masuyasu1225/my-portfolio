195e1c1f2488a0e0b285e3c7b5ff83c133e0c692
masu <yasumoto.7991mm@gmail.com>
2023-09-09 21:18:41 +0900

    Update footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,20 +4,6 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer-container">
-      <ul className="footer-links">
-        <li>
-          <a href="#about">About Us</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-        <li>
-          <a href="#terms">Terms of Service</a>
-        </li>
-        <li>
-          <a href="#privacy">Privacy Policy</a>
-        </li>
-      </ul>
       <div className="footer-copyright">
         &copy; {new Date().getFullYear()} Motoyasu Masui. All Rights Reserved.
       </div>
