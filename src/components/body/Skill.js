ddb1d25d5436951e9f044ee3ebc9165c541e6e96
masu <yasumoto.7991mm@gmail.com>
2023-09-12 20:35:31 +0900

    Update skill card

--- src/components/body/Skill.js
+++ src/components/body/Skill.js
@@ -17,41 +17,49 @@ const Skill = () => (
           <img src={javaLogo} alt="java" />
           <div>Java</div>
           <div>経験年数：4年</div>
+          <div>(大学、実務)</div>
         </div>
         <div className="card">
           <img src={javaScriptLogo} alt="javaScript" />
           <div>JavaScript</div>
           <div>経験年数：1年</div>
+          <div>(独学)</div>
         </div>
         <div className="card">
           <img src={reactLogo} alt="react" />
           <div>React</div>
           <div>経験年数：1年</div>
+          <div>(独学)</div>
         </div>
         <div className="card">
           <img src={cLanguageLogo} alt="clanguage" />
           <div>C</div>
           <div>経験年数：1年</div>
+          <div>(大学)</div>
         </div>
         <div className="card">
           <img src={pythonLogo} alt="python" />
           <div>Python</div>
           <div>経験年数：1年</div>
+          <div>(大学、独学)</div>
         </div>
         <div className="card">
           <img src={postgresqlLogo} alt="postgresql" />
           <div>PostgreSQL</div>
           <div>経験年数：1年</div>
+          <div>(大学、実務)</div>
         </div>
         <div className="card">
           <img src={mysqlLogo} alt="mysql" />
           <div>MySQL</div>
           <div>経験年数：3か月</div>
+          <div>(実務)</div>
         </div>
         <div className="card">
           <img src={springLogo} alt="spring" />
-          <div>SpringL</div>
+          <div>Spring</div>
           <div>経験年数：3か月</div>
+          <div>(実務)</div>
         </div>
       </div>
     </div>
